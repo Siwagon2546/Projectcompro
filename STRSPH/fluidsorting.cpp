1ffca7dbe91b3f82ca4c3c94a49b7f3fce887a55
STRSPH <nahmpiyanan@gmail.com>
2023-02-26 04:32:47 +0700

    2dvector

--- STRSPH/fluidsorting.cpp
+++ STRSPH/fluidsorting.cpp
@@ -1,12 +1,5 @@
 #include "fluidsortingHeader.h"
 
 int main(){
-    srand(time(0));
-    fluidsort game;
-    game.levelparameter(4);
-    cout << game.Nbottle << " + " << game.Nemtybottle << "\t" << game.Nfluidtype << endl << endl ;
-    game.createbottle();
-    cout << endl << endl;
-    game.fillbottle();
-    
+    test(3);
 }
--- STRSPH/fluidsortingheader.h
+++ STRSPH/fluidsortingheader.h
@@ -15,12 +15,12 @@ class fluidsort{
     int Nemtybottle;
     int Nfluidtype;
     vector<int> bottle;
-    vector<int> type;
+    vector<vector<int>> bottle2dvector;
 
     void levelparameter(int); //get Nbottle,Nemtybottle,Nfluidtype form input(lvl of stage)
-    void createbottle(); //create array (row = Nbottle , col = Nfluditype)
-    void fillbottle();
+    void createandfillbottle(); //create array (row = Nbottle , col = Nfluditype) and fillbottle randomly
     void createstage();
+    void vector22dvector();
 };
 
 void fluidsort::levelparameter(int stagelvl){
@@ -48,42 +48,60 @@ void fluidsort::levelparameter(int stagelvl){
     }
 }
 
-void fluidsort::createbottle(){
-    for(int i=0;i<(Nbottle + Nemtybottle)*Nfluidtype;i+=1){
+void fluidsort::createandfillbottle(){
+    for(int i=1;i<Nbottle+1;i+=1){
+        for(int j=0;j<Nfluidtype;j+=1){
+            bottle.push_back(i);
+        } }
+    for(int i=0;i<Nemtybottle*Nfluidtype;i+=1){
         bottle.push_back(0);
     }
 
+    random_shuffle(bottle.begin(),bottle.end());
+
     for(int i=0;i<(Nbottle + Nemtybottle)*Nfluidtype;i+=1){
-            if(i !=0 && i%Nfluidtype==0){cout << endl;}
+        if(i !=0 && i%Nfluidtype==0){cout << endl;}
             cout << bottle[i] << "\t";
     }
 }
 
-void fluidsort::fillbottle(){
-    for(int i=1;i<Nbottle+1;i+=1){
-        for(int j=0;j<Nfluidtype;j+=1){
-            type.push_back(i);
-        } }
-    for(int i=0;i<Nemtybottle*Nfluidtype;i+=1){
-        type.push_back(0);
-    }
+void fluidsort::vector22dvector(){
+    /*
+    for(int i=0;i<(Nbottle + Nemtybottle)*Nfluidtype;i=+1){
+        bottle2dvector[i/Nfluidtype][i%Nfluidtype] = bottle[i];    //อันนี้ใช้ไม่ได้เฉย
+    }*/
+    bottle2dvector.resize(Nbottle+Nemtybottle ,bottle);
     
-    random_shuffle(type.begin(),type.end());
+    int k =0;
+    for(int i=0;i<Nbottle + Nemtybottle;i+=1){
+        for(int j=0;j<Nfluidtype;j+=1){
+            bottle2dvector[i][j] = bottle[k];
+            k+=1;
+        }
+    }
 
-    for(int i=0;i<(Nbottle + Nemtybottle)*Nfluidtype;i+=1){
-        if(i !=0 && i%Nfluidtype==0){cout << endl;}
-            cout << type[i] << "\t";
+    for(int i=0;i<Nbottle + Nemtybottle;i+=1){
+        for(int j=0;j<Nfluidtype;j+=1){
+            cout << bottle2dvector[i][j] << "\t" ;
+        }
+        cout << endl;
     }
 }
 
 void fluidsort::createstage(){
+     //Sleep(1000); //Delay (ms)
+    //system("cls"); //Screenclear
     cout<< " ";
 }
 
-void test(){
-    cout << "test " ;
-    Sleep(1000); //Delay (ms)
-    system("cls"); //Screenclear
+void test(int lvl){
+    srand(time(0));
+    fluidsort game;
+    game.levelparameter(lvl);
+    cout << game.Nbottle << " + " << game.Nemtybottle << "\t" << game.Nfluidtype << endl << endl ;
+    game.createandfillbottle();
+    cout << endl<<endl;
+    game.vector22dvector();
 }
 
 
