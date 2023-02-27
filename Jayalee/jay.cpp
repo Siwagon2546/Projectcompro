7210881a3953f8995dc0f66e23574e92f1e4d285
JayAleecha <aleecha.2546@gmail.com>
2023-02-27 11:38:55 +0700

    test1

---
+++ Jayalee/jay.cpp
@@ -0,0 +1,26 @@
+#include<iostream>                                 
+#include<>
+#include<>
+#include<>
+#include<>
+void Gosnake(){
+    
+}
+void fruitX(){
+
+}
+void fruitY(){
+
+}
+double sumfruit(){
+
+}
+void checkAns(){
+
+}
+void monitor(){
+
+}
+int main(){
+    
+}
