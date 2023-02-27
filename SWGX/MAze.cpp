d6aeaa5f8b0ce5c483d405d6bf53349c33e44d18
Siwagon2546 <netevgjqw@gmail.com>
2023-02-27 23:53:27 +0700

    up1

--- SWGX/MAze.cpp
+++ SWGX/MAze.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
+const int W = 70;
+
+void Maze(char [][W],int,int);
+void showMaze(char [][W],int,int);
+
 int main(){
-    cout<<"Hello world";
+    char Maze_l[][W]={};
+    Maze(Maze_l,10,10);
+    showMaze(Maze_l,10,10);
+}
+
+void Maze(char Mazed[][W],int H,int W){
+    
+    for(int i=0;i<H;i++){
+        for(int j=0;j<W;j++){
+            Mazed[i][j]='#';
+        }
+
+    }
+}
+
+void showMaze(char Mazed[][W],int Height,int Width){
+    for(int i=0;i<Height;i++){
+        for(int j=0;j<Width;j++){
+            cout<<Mazed[i][j];
+        }
+        cout<<endl;
+    }
 
 }
