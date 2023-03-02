ae87000df56d831c831da811203c5d062ec7e6a9
Siwagon2546 <netevgjqw@gmail.com>
2023-03-02 18:17:15 +0700

    update

--- SWGX/MAze.cpp
+++ SWGX/MAze.cpp
@@ -1,34 +1,92 @@
 #include <iostream>
+#include <conio.h> // for getch() function
 
 using namespace std;
 
-const int W = 70;
+const int ROWS = 10;
+const int COLS = 20;
 
-void Maze(char [][W],int,int);
-void showMaze(char [][W],int,int);
+// The maze is represented as a 2D array of characters
+char maze[ROWS][COLS] = {
+    {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+    {'#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+    {'#', ' ', '#', ' ', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#'},
+    {'#', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#'},
+    {'#', ' ', '#', '#', '#', '#', ' ', '#', ' ', '#', ' ', '#', '#', '#', ' ', '#', '#', '#', ' ', '#'},
+    {'#', ' ', ' ', ' ', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+    {'#', '#', '#', '#', ' ', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#', '#', '#', '#', '#', ' ', '#'},
+    {'#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', '#'},
+    {'#', ' ', '#', '#', '#', ' ', '#', '#', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', '#', ' ', '#'},
+    {'#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#'},
+};
 
+// The player's position
+int playerRow = 1;
+int playerCol = 1;
+
+// The goal position
+int goalRow = ROWS - 2;
+int goalCol = COLS - 2;
+
+// Draw the maze
+void drawMaze() {
+    for (int row = 0; row < ROWS; row++) {
+        for (int col = 0; col < COLS; col++) {
+            if (row == playerRow && col == playerCol) {
+                cout << "P"; // draw the player
+            } else if (row == goalRow && col == goalCol) {
+                cout << "G"; // draw the goal
+            } else {
+                cout << maze[row][col]; // draw the maze
+            }
+        }
+        cout << endl;
+    }
+}
+
+// Move the player
+void movePlayer(int rowOffset, int colOffset) {
+    int newRow = playerRow + rowOffset;
+    int newCol = playerCol + colOffset;
+    if (maze[newRow][newCol] == ' ') {
+        playerRow = newRow;
+        playerCol = newCol;
+    }
+}
+    // Game loop
 int main(){
-    char Maze_l[][W]={};
-    Maze(Maze_l,10,10);
-    showMaze(Maze_l,10,10);
-}
+while (true) {
+    // Draw the maze
+    char input;
+    system("cls"); // clear the console
+    drawMaze();
 
-void Maze(char Mazed[][W],int H,int W){
-    
-    for(int i=0;i<H;i++){
-        for(int j=0;j<W;j++){
-            Mazed[i][j]='#';
-        }
+    // Get input from the player
+    input = getch();
+    switch (input) {
+        case 'w':
+            movePlayer(-1, 0);
+            break;
+        case 'a':
+            movePlayer(0, -1);
+            break;
+        case 's':
+            movePlayer(1, 0);
+            break;
+        case 'd':
+            movePlayer(0, 1);
+            break;
+        case 'q':
+            return 0; // quit the game
+        default:
+            break;
+    }
 
+    // Check if the player has reached the goal
+    if (playerRow == goalRow && playerCol == goalCol) {
+        cout << "You win!" << endl;
+        break;
     }
 }
-
-void showMaze(char Mazed[][W],int Height,int Width){
-    for(int i=0;i<Height;i++){
-        for(int j=0;j<Width;j++){
-            cout<<Mazed[i][j];
-        }
-        cout<<endl;
-    }
-
+return 0;
 }
