6a1022a0a16f707523ed3df3fdbd500ae6e6a876
JayAleecha <aleecha.2546@gmail.com>
2023-03-02 18:47:30 +0700

    jay.cpp

--- Jayalee/jay.cpp
+++ Jayalee/jay.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
 #include<conio.h>
+#include<vector>
+#include<windows.h>
+#include<ctime>
+
 using namespace std;
-void controllsnake(){
-    
+void snakecreate(){
+    int width = 40;
+    int height = 20;
+vector<vector<char>> rectangle(height, vector<char>(width, ' '));
+
+for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      if(i==0 or i==height-1){
+        rectangle[i][j]='_';
+        }else{
+          if(j==0 or j==width-1){
+            rectangle[i][j]='|';
+          }
+        }
+    }
+}
+  
+for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      cout << rectangle[i][j];
+    }
+    cout << endl;
+}
+}
+//void controllsnake(){
+    /*
     unsigned int waited_MS=0 , input_wait_MS=10;
     do{
         if(_kbhit()){
@@ -13,8 +41,8 @@ void controllsnake(){
                 case '77' : snake.set_face(Snake::RIGHT); break;
             }
         }
-    }
-
+    }*/
+/*
 }
 void fruitX(){
 
@@ -22,7 +50,7 @@ void fruitX(){
 void fruitY(){
 
 }
-double sumfruit(){
+void sumfruit(){
 
 }
 void checkAns(){
@@ -30,7 +58,17 @@ void checkAns(){
 }
 void monitor(){
 
-}
+}*/
 int main(){
+    srand(time(0));
+    int num = rand()%100 + 1;
+    while (true){
+        system("cls");
+        
+        cout << "          SMART snake Game   \n";  
+        cout << "            Number is " <<    num    << "\n";
+        snakecreate();
+        //Sleep(20);
+        }
     
 }
