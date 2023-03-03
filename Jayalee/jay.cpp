6606335726f8af3ce301c3740543c4da5c9cc6b3
JayAleecha <aleecha.2546@gmail.com>
2023-03-03 21:55:26 +0700

    jay.cpp

--- Jayalee/jay.cpp
+++ Jayalee/jay.cpp
@@ -5,30 +5,52 @@
 #include<ctime>
 
 using namespace std;
-void snakecreate(){
+void snakecreate(int num){
     int width = 40;
     int height = 20;
+    srand(time(0));
+    int locationX[5] = {(rand()%(width-1))+1,(rand()%(width-1))+1,(rand()%(width-1))+1,(rand()%(width-1))+1,(rand()%(width-1))+1};
+    int locationY[5] = {(rand()%(height-1))+1,(rand()%(height-1))+1,(rand()%(height-1))+1,(rand()%(height-1))+1,(rand()%(height-1))+1};
+    //locationX[4] != locationX[3] != locationX[2] != locationX[1] != locationX[0];
+    //locationY[4] != locationY[3] != locationY[2] != locationY[1] != locationY[0];
+    cout << "          SMART Snake Game   \n";
+    cout << "            Number is " <<    num    << "\n";
+
 vector<vector<char>> rectangle(height, vector<char>(width, ' '));
 
-for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      if(i==0 or i==height-1){
-        rectangle[i][j]='_';
-        }else{
-          if(j==0 or j==width-1){
-            rectangle[i][j]='|';
-          }
+
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+        if(i==0 or i==height-1){
+            rectangle[i][j]='_';
+            }else{
+            if(j==0 or j==width-1){
+                rectangle[i][j]='|';
+            }
+            }
         }
     }
-}
-  
-for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      cout << rectangle[i][j];
+
+    for(int k = 0 ; k < 5 ; k++){
+        for(int i = 0 ; i < height-1 ; i++){
+            for(int j = 0 ; j < width-1 ; j++){
+                if(locationX[k] == j && locationY[k] == i){
+                    rectangle[i][j]='*';
+                }
+            }
+        }
     }
-    cout << endl;
+    
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+        cout << rectangle[i][j];
+        }
+        cout << endl;
+    }
+    
 }
-}
+
 //void controllsnake(){
     /*
     unsigned int waited_MS=0 , input_wait_MS=10;
@@ -41,16 +63,9 @@ for (int i = 0; i < height; i++) {
                 case '77' : snake.set_face(Snake::RIGHT); break;
             }
         }
-    }*/
-/*
-}
-void fruitX(){
-
-}
-void fruitY(){
-
-}
-void sumfruit(){
+    }
+}*/
+/*void sumfruit(){
 
 }
 void checkAns(){
@@ -63,12 +78,10 @@ int main(){
     srand(time(0));
     int num = rand()%100 + 1;
     while (true){
-        system("cls");
-        
-        cout << "          SMART snake Game   \n";  
-        cout << "            Number is " <<    num    << "\n";
-        snakecreate();
+    system("cls");
+    snakecreate(num);       
+    }    
         //Sleep(20);
-        }
+    return 0;
     
 }
